ae4c25f131a09d1fda97e4065fbbc8947707b2f7
G Brown (sysdevone) <sysdevone@users.noreply.github.com>
2015-04-01 23:42:33 -0700

    initial checkin for words

---
+++ src/main/java/org/gabsocial/collection/Words.java
@@ -0,0 +1,142 @@
+/*****************************************************************************************
+ * 
+ * Copyright 2015 Gregory Brown. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ ***************************************************************************************** 
+ */
+
+package org.gabsocial.collection;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.gabsocial.gabdev.validate.Validate;
+
+
+/**
+ * This is a word container. It is designed to help get suggestions for
+ * misspellings.
+ * 
+ * @author Gregory Brown (sysdevone)
+ * 
+ */
+public class Words
+{
+    /*
+     * Uses a tree to help determine suggestions.
+     */
+    private final Tree<String> _tree;
+    
+    /**
+     * Default constructor.
+     */
+    public Words()
+    {
+        this._tree = new Tree<String>("root");
+    }
+    
+    /**
+     * Add a word to the container that will be used as suggestions.
+     * 
+     * @param word
+     *            A <code>String</code> instance. May not be null or empty.
+     */
+    public void add(final String word)
+    {
+        Validate.isNotNullOrEmpty(this.getClass(),
+                "The parameter 'word' should not be null.", word);
+        
+        final int count = word.length();
+        Tree.Node<String> node = this._tree.getRoot();
+        for (int i = 0; i < count; ++i)
+        {
+            // break the word into characters and add each character to the
+            // tree.
+            // A character will be a child of the previous character.
+            // The complete word is the final leaf.
+            final String character = Character.toString(word.charAt(i));
+            if (node.containsChild(character))
+            {
+                // if the character is contained, get that node so that further
+                // characters can be added to it.
+                node = node.getChild(character);
+            }
+            else
+            {
+                // if the character is not contained, then create a new node so
+                // that further characters can be added to it.
+                node = node.addChild(character);
+            }
+        }
+        node = node.addChild(word);
+    }
+    
+    /**
+     * Gets suggestions for a word.
+     * 
+     * @param word
+     *            A <code>String</code> instance. May not be null or empty.
+     * @return A <code>List</code> instance containing zero to many
+     *         <code>String</code> instances.
+     */
+    public List<String> getSuggestions(final String word)
+    {
+        Validate.isNotNullOrEmpty(this.getClass(),
+                "The parameter 'word' should not be null.", word);
+        
+        //
+        // Converts the word to characters.
+        // Tests to see if the character exists in the tree.
+        // -------------------
+        final LinkedList<String> data = new LinkedList<String>();
+        final int count = word.length();
+        Tree.Node<String> node = this._tree.getRoot();
+        for (int i = 0; i < count; ++i)
+        {
+            final String character = Character.toString(word.charAt(i));
+            if (node.containsChild(character))
+            {
+                // if the character exists, then get that node.
+                // continue walking down the tree character by character.
+                node = node.getChild(character);
+            }
+            else
+            {
+                // if the character is not found. STOP.
+                break;
+            }
+        }
+        
+        // if the node is not the root, get all the leaf nodes as a
+        // suggestion.
+        if (!node.isRoot())
+        {
+            node.getLeafData(data);
+        }
+        
+        return (data);
+    }
+    
+    /**
+     * Gets the words that were added.
+     * 
+     * @return A <code>List</code> instance containing zero to many
+     *         <code>String</code> instances.
+     */
+    public List<String> getWords()
+    {
+        return (this._tree.getLeafData());
+    }
+}
---
+++ src/test/java/org/gabsocial/collection/WordsTest.java
@@ -0,0 +1,108 @@
+/*****************************************************************************************
+ * 
+ * Copyright 2015 Gregory Brown. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ ***************************************************************************************** 
+ */
+
+package org.gabsocial.collection;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * 
+ * 
+ * 
+ * @author Gregory Brown (sysdevone)
+ * 
+ */
+public class WordsTest
+{
+    
+    public static final void main(final String[] args)
+    {
+        final WordsTest test = new WordsTest();
+        try
+        {
+            test.initialize();
+            test.testSuggestion();
+        }
+        finally
+        {
+            test.close();
+        }
+    }
+    
+    @After
+    public void close()
+    {
+        
+    }
+    
+    @Before
+    public void initialize()
+    {
+        
+    }
+    
+    @Test
+    public void testSuggestion()
+    {
+        Words suggestion = new Words();
+        suggestion.add("helloworld");
+        suggestion.add("hello");
+        suggestion.add("hey");
+        suggestion.add("world");
+        
+        List<String> words = suggestion.getSuggestions("hell012222222");
+        org.junit.Assert.assertTrue(words.contains("hello"));
+        org.junit.Assert.assertTrue(words.contains("helloworld"));
+        org.junit.Assert.assertEquals(2, words.size());
+        
+        // for (String word : words)
+        // {
+        // System.out.println("Suggestion Word: " + word);
+        // }
+        //
+        // words = suggestion.getWords();
+        // for (String word : words)
+        // {
+        // System.out.println("Word: " + word);
+        // }
+    }
+    
+    @Test
+    public void testWords()
+    {
+        Words suggestion = new Words();
+        suggestion.add("helloworld");
+        suggestion.add("hello");
+        suggestion.add("hey");
+        suggestion.add("world");
+        
+        List<String> words = suggestion.getWords();
+        org.junit.Assert.assertTrue(words.contains("hello"));
+        org.junit.Assert.assertTrue(words.contains("helloworld"));
+        org.junit.Assert.assertTrue(words.contains("hey"));
+        org.junit.Assert.assertTrue(words.contains("world"));
+        org.junit.Assert.assertEquals(4, words.size());
+    }
+    
+}
